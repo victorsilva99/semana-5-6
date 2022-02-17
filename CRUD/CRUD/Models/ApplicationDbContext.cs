3d6b2af97deec9128d1dfc9d19c87b546062c3f7
Victor Silva de Souza <fe605@t-systems.com.br>
2022-02-16 23:32:37 -0300

    Initial commit

---
+++ CRUD/CRUD/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<Caminhao> Caminhao { get; set; }
+    }
+}
---
+++ CRUD/CRUD/Models/Caminhao.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Caminhao
+    {
+        [Key]
+        [Required(ErrorMessage = "Informe a ID do caminhão.")]
+        [Range(1, int.MaxValue, ErrorMessage = "O ID não pode ser nulo (0) ou um número negativo.")]
+        [Display(Name ="ID do caminhão")]
+        public int IdCaminhao { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do caminhão.")]
+        [Display(Name ="Nome do caminhão")]
+        [StringLength(50, ErrorMessage = "Limite máximo de caracteres foi excedido (50).")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Descrição do caminhão")]
+        [StringLength(100, ErrorMessage = "Limite máximo de caracteres foi excedido (100).")]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Data da criação")]
+        [Required(ErrorMessage = "Informe a data de criação.")]
+        [DataType(DataType.Date)]
+        public DateTime DataCriacao { get; set; }
+
+    }
+}
